8b8198e8eaa7cf294d41d1e9ce7e237fc97b1085
Reina Uhler <reinadu@gmail.comgit>
2020-10-13 15:52:44 -0700

    added counter red and disable button  add imagery and answer

--- script.js
+++ script.js
@@ -14,62 +14,126 @@ $("#sign-up-button").click(function () {
    $("#sign-up-card").toggle();
    $("#sign-up-button").toggleClass("d-flex d-none");
 });
-
-let createImageryInputCharsCount = 0;
-
-$("#create-imagery-input").keydown(function (e) {
-   const key = e.which;
-   console.log(key);
-
-   // If the key the user pressed is a backspace, decrement the count
-   if (key === 8) {
-      console.log("the user pressed backspace");
-      createImageryInputCharsCount--;
-      if (createImageryInputCharsCount < 0) {
-         console.log("you are negative");
-         createImageryInputCharsCount = 0;
-      }
-   } else if (key === 16) {
-      console.log("shift was pressed.");
-   } else if (key === 18) {
-      console.log("alt was pressed.");
-   } else if (key === 91) {
-      console.log("command");
+// create-imagery charachter counter
+$("#create-imagery-input").keyup(function (e) {
+   // get the text from the textarea
+   const text = e.target.value;
+   // check the length of the text
+   const textLength = text.length;
+   if (textLength > 240 || textLength < 1) {
+      document.getElementById("save-card").disabled = true;
+      document.getElementById("char-count").classList.add("text-danger");
+      document.getElementById("char-count").classList.remove("text-muted");
    } else {
-      // else, increment the count
-      console.log("the user pressed any other key");
-      createImageryInputCharsCount += 1;
+      document.getElementById("save-card").disabled = false;
+      document.getElementById("char-count").classList.add("text-muted");
    }
-   console.log("Total inoutted chars: ", createImageryInputCharsCount);
-   $("#imagery-char-count").html(createImageryInputCharsCount);
+   //update the character counter on the page
+   $("#imagery-char-count").html(textLength);
 });
 
-let createAnswerInputCharsCount = 0;
+// create-answer charachter counter
+$("#create-answer-input").keyup(function (e) {
+   // get the text from the textarea
+   const text = e.target.value;
 
-$("#create-answer-input").keypress(function () {
-   console.log("let's add 1");
-   createAnswerInputCharsCount += 1;
+   // check the length of the text
+   const textLength = text.length;
+   if (textLength > 240 || textLength < 1) {
+      document.getElementById("save-card").disabled = true;
+      document.getElementById("char-count").classList.add("text-danger");
+      document.getElementById("char-count").classList.remove("text-muted");
+   } else {
+      document.getElementById("save-card").disabled = false;
+      document.getElementById("char-count").classList.add("text-muted");
+   }
 
-   console.log("Total inputted chars: ", createAnswerInputCharsCount);
-   $("#answer-char-count").html(createAnswerInputCharsCount);
+   // if (textLength > 240 || textLength < 1) {
+   //    document.getElementById("save-card").disabled = true;
+   //    $("#create-answer-char").toggleClass("text-danger text-muted");
+   // } else {
+   //    document.getElementById("save-card").disabled = false;
+   // }
+   //update the character counter on the page
+
+   $("#answer-char-count").html(textLength);
 });
 
-let editImageryInputCharsCount = 0;
+// let createAnswerInputCharsCount = 0;
 
-$("#edit-imagery-input").keypress(function () {
-   console.log("let's add 1");
-   editImageryInputCharsCount += 1;
+// $("#create-answer-input").keypress(function () {
+//    console.log("let's add 1");
+//    createAnswerInputCharsCount += 1;
 
-   console.log("Total inputted chars: ", editImageryInputCharsCount);
-   $("#edit-imagery-char-count").html(editImageryInputCharsCount);
-});
+//    console.log("Total inputted chars: ", createAnswerInputCharsCount);
+//    $("#answer-char-count").html(createAnswerInputCharsCount);
+// });
 
-let editAnswerInputCharsCount = 0;
+// let editImageryInputCharsCount = 0;
 
-$("#edit-answer-input").keypress(function () {
-   console.log("let's add 1");
-   editAnswerInputCharsCount += 1;
+// $("#edit-imagery-input").keypress(function () {
+//    console.log("let's add 1");
+//    editImageryInputCharsCount += 1;
 
-   console.log("Total inputted chars: ", editAnswerInputCharsCount);
-   $("#edit-answer-char-count").html(editAnswerInputCharsCount);
+//    console.log("Total inputted chars: ", editImageryInputCharsCount);
+//    $("#edit-imagery-char-count").html(editImageryInputCharsCount);
+// });
+
+// let editAnswerInputCharsCount = 0;
+
+// $("#edit-answer-input").keypress(function () {
+//    console.log("let's add 1");
+//    editAnswerInputCharsCount += 1;
+
+//    console.log("Total inputted chars: ", editAnswerInputCharsCount);
+//    $("#edit-answer-char-count").html(editAnswerInputCharsCount);
+// });
+
+// edit-imagery charachter counter
+// $("#edit-imagery-input").keyup(function (e) {
+//    console.log("Event: ", e);
+
+//    // get the text from the textarea
+//    const text = e.target.value;
+
+//    // check the length of the text
+//    const textLength = text.length;
+
+//    if (textLength > 240 || textLength < 1) {
+//       console.log("you are over 250 characters");
+
+//       document.getElementById("save-card").disabled = true;
+//       $("#Top-Bottom-Counter").toggleClass("text-danger text-muted");
+//    } else {
+//       document.getElementById("save-card").disabled = false;
+//    }
+
+//    //update the character counter on the page
+
+//    $("#edit-imagery-char-count").html(textLength);
+// });
+
+// edit-answer charachter counter
+$("#edit-answer-input, #edit-imagery-input").keyup(function (e) {
+   console.log("Event: ", e);
+
+   // get the text from the textarea
+   const text = e.target.value;
+
+   // check the length of the text
+   const textLength2 = text.length;
+   const textLength = text.length;
+
+   if (textLength2 > 240 || textLength2 < 1) {
+      console.log("you are over 250 characters");
+
+      document.getElementById("save-card").disabled = true;
+      $("#Top-Bottom-Counter").toggleClass("text-danger text-muted");
+   } else {
+      document.getElementById("save-card").disabled = false;
+   }
+
+   //update the character counter on the page
+   $("#edit-imagery-char-count").html(textLength);
+   $("#edit-answer-char-count").html(textLength2);
 });

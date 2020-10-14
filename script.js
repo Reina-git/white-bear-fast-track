58844f6c545eed9aaf9498983a078d7c8a4c8711
Reina Uhler <reinadu@gmail.comgit>
2020-10-14 12:58:19 -0700

    added email password verification

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ $("#create-answer-input").keyup(function (e) {
    // check the length of the text
    const textLength = text.length;
    if (textLength > 240 || textLength < 1) {
-      document.getElementById("save-card").disabled = true;
+      $("#save-card").addClass("disabled");
       document.getElementById("char-count").classList.add("text-danger");
       document.getElementById("char-count").classList.remove("text-muted");
    } else {
@@ -48,48 +48,117 @@ $("#create-answer-input").keyup(function (e) {
       document.getElementById("char-count").classList.add("text-muted");
    }
 
-   // if (textLength > 240 || textLength < 1) {
-   //    document.getElementById("save-card").disabled = true;
-   //    $("#create-answer-char").toggleClass("text-danger text-muted");
-   // } else {
-   //    document.getElementById("save-card").disabled = false;
-   // }
-   //update the character counter on the page
-
    $("#answer-char-count").html(textLength);
 });
 
-// let createAnswerInputCharsCount = 0;
-
-// $("#create-answer-input").keypress(function () {
-//    console.log("let's add 1");
-//    createAnswerInputCharsCount += 1;
-
-//    console.log("Total inputted chars: ", createAnswerInputCharsCount);
-//    $("#answer-char-count").html(createAnswerInputCharsCount);
+// $("#edit-cards-total").log(function (e) {
+//    $("#edit-imagery-input").keyup(function (e) {
+//       console.log("Event: ", e);
+//       const text = e.target.value;
+//       $("#edit-imagery-char-count").html(textLength);
+//    });
 // });
 
-// let editImageryInputCharsCount = 0;
+// $("#edit-imagery-input, #edit-answer-input").keyup(function () {
+//    const imageryInput = $("#edit-imagery-input").val();
+//    const answerInput = $("#edit-answer-input").val();
+//    const imageryTextLength = text.length;
+//    const answerTextLength = text.length;
 
-// $("#edit-imagery-input").keypress(function () {
-//    console.log("let's add 1");
-//    editImageryInputCharsCount += 1;
-
-//    console.log("Total inputted chars: ", editImageryInputCharsCount);
-//    $("#edit-imagery-char-count").html(editImageryInputCharsCount);
+//    $("#edit-answer-char-count").html(answerTextLength);
+//    $("#edit-imagery-char-count").html(imageryTextLength);
 // });
 
-// let editAnswerInputCharsCount = 0;
+$("#edit-imagery-input, #edit-answer-input").keyup(function () {
+   const imageryInput = $("#edit-imagery-input").val();
+   const answerInput = $("#edit-answer-input").val();
+   if (imageryInput.length > 240) {
+      $("#edit-imagery-char-count").addClass("text-danger");
+   } else {
+      $("#edit-imagery-char-count").removeClass("text-danger");
+   }
+   if (answerInput.length > 240) {
+      $("#edit-answer-char-count").addClass("text-danger");
+   } else {
+      $("#edit-answer-char-count").removeClass("text-danger");
+   }
+   if (
+      imageryInput.length > 0 &&
+      imageryInput.length <= 240 &&
+      answerInput.length > 0 &&
+      answerInput.length <= 240
+   ) {
+      $("#edit-save-card").removeClass("disabled");
+   } else {
+      $("#edit-save-card").addClass("disabled");
+   }
+   $("#edit-imagery-char-count").html(imageryInput.length);
+   $("#edit-answer-char-count").html(answerInput.length);
+});
 
-// $("#edit-answer-input").keypress(function () {
-//    console.log("let's add 1");
-//    editAnswerInputCharsCount += 1;
+$("#letsGoButton").click(function () {
+   const emailInput = $("#email-sign-up").val();
+   const passwordInput = $("#password-sign-up").val();
 
-//    console.log("Total inputted chars: ", editAnswerInputCharsCount);
-//    $("#edit-answer-char-count").html(editAnswerInputCharsCount);
+   if (emailInput.length < 1) {
+      $("#enterEmail").removeClass("d-none");
+      $("#email-sign-up").addClass("is-invalid");
+   } else {
+      $("#enterEmail").addClass("d-none");
+      $("#email-sign-up").removeClass("is-invalid");
+   }
+
+   if (passwordInput.length === 0) {
+      $("#enterPassword").removeClass("d-none");
+      $("#password-sign-up").addClass("is-invalid");
+   } else if (passwordInput.length < 9 && passwordInput.length > 0) {
+      $("#passwordLenth").removeClass("d-none");
+      $("#password-sign-up").addClass("is-invalid");
+      $("#enterPassword").addClass("d-none");
+   } else {
+      $("#enterPassword").addClass("d-none");
+      $("#passwordLenth").addClass("d-none");
+      $("#password-sign-up").removeClass("is-invalid");
+   }
+});
+
+// $("#letsGoButton").click(function () {
+//    const isEmailInvalid = getEmailError(email);
+//    if (isEmailInvalid) {
+//       showErrorValidation(
+//          "#new-user-email-error",
+//          "Please enter your email address."
+//       );
+//       $("#email-sign-up").addClass("is-invalid");
+//    } else {
+//       $("#email-sign-up").removeClass("is-invalid");
+//       $("#email-sign-up").addClass("is-valid");
+//       showErrorValidation("#new-user-email-error", "");
+//    }
 // });
 
-// edit-imagery charachter counter
+// $("#email-sign-up").keyup(function (e) {
+//    const text = e.target.value;
+//    const textLegth = text.length;
+
+//    if (textLength < 0) {
+//       $("letsGoButton").addClass("disabled");
+//    }
+// });
+
+// $("#edit-imagery.input".keyup(function(e) {
+//    const imageryTextLength = $("#edit-imagery-char-count").val()
+//    const answerTextLength = $("#edit-answer-char-count").val()
+//    // do stuff with these values
+
+//    $("#Top-Bottom-Counter").html(function () {
+
+// });
+
+// }
+// });
+
+// // edit-imagery charachter counter
 // $("#edit-imagery-input").keyup(function (e) {
 //    console.log("Event: ", e);
 
@@ -113,27 +182,27 @@ $("#create-answer-input").keyup(function (e) {
 //    $("#edit-imagery-char-count").html(textLength);
 // });
 
-// edit-answer charachter counter
-$("#edit-answer-input, #edit-imagery-input").keyup(function (e) {
-   console.log("Event: ", e);
+// // edit-answer charachter counter
+// $("#edit-answer-input, #edit-imagery-input").keyup(function (e) {
+//    console.log("Event: ", e);
 
-   // get the text from the textarea
-   const text = e.target.value;
+//    // get the text from the textarea
+//    const text = e.target.value;
 
-   // check the length of the text
-   const textLength2 = text.length;
-   const textLength = text.length;
+//    // check the length of the text
+//    const textLength2 = text.length;
+//    const textLength = text.length;
 
-   if (textLength2 > 240 || textLength2 < 1) {
-      console.log("you are over 250 characters");
+//    if (textLength2 > 240 || textLength2 < 1) {
+//       console.log("you are over 250 characters");
 
-      document.getElementById("save-card").disabled = true;
-      $("#Top-Bottom-Counter").toggleClass("text-danger text-muted");
-   } else {
-      document.getElementById("save-card").disabled = false;
-   }
+//       document.getElementById("save-card").disabled = true;
+//       $("#Top-Bottom-Counter").toggleClass("text-danger text-muted");
+//    } else {
+//       document.getElementById("save-card").disabled = false;
+//    }
 
-   //update the character counter on the page
-   $("#edit-imagery-char-count").html(textLength);
-   $("#edit-answer-char-count").html(textLength2);
-});
+//    //update the character counter on the page
+//    $("#edit-imagery-char-count").html(textLength);
+//    $("#edit-answer-char-count").html(textLength2);
+// });

405e4bac4dfec24410a6a5d99b0d1d20b58053c1
Reina Uhler <reinadu@gmail.comgit>
2020-10-15 16:47:33 -0700

    up to video 126A

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $("#save-card").click(function () {
    $("#overlay-success").toggleClass("d-flex d-none");
-   console.log("Here are the most insecure passwords:", mostInsecurePasswords);
+   // console.log("Here are the most insecure passwords:", mostInsecurePasswords);
 });
-
+// make the error message show up and go away when clicked
 $("#back-card").click(function () {
    $("#overlay-error").toggleClass("d-flex d-none");
 });
@@ -60,16 +60,6 @@ $("#create-answer-input").keyup(function (e) {
 //    });
 // });
 
-// $("#edit-imagery-input, #edit-answer-input").keyup(function () {
-//    const imageryInput = $("#edit-imagery-input").val();
-//    const answerInput = $("#edit-answer-input").val();
-//    const imageryTextLength = text.length;
-//    const answerTextLength = text.length;
-
-//    $("#edit-answer-char-count").html(answerTextLength);
-//    $("#edit-imagery-char-count").html(imageryTextLength);
-// });
-
 $("#edit-imagery-input, #edit-answer-input").keyup(function () {
    const imageryInput = $("#edit-imagery-input").val();
    const answerInput = $("#edit-answer-input").val();
@@ -108,13 +98,43 @@ $("#letsGoButton").click(function () {
    const delimiter = `@`;
    const indexOfEmail = lowerCasedEmail.indexOf(delimiter);
    const localEmail = emailInput.slice(0, indexOfEmail);
+
    // console.log(`the local part of ${emailInput} is ${localEmail}.`);
 
+   // combine mostInsecurePasswords and secondMostInsecurePasswords
+
+   const allPasswords = [
+      ...mostInsecurePasswords,
+      ...secondMostInsecurePasswords,
+   ].flat();
+   const allUniqPasswords = [...new Set(allPasswords)];
+
+   const firstPasswords = allUniqPasswords.slice(
+      0,
+      allUniqPasswords.indexOf("skywalker")
+   );
+
+   const secondPasswords = allUniqPasswords.slice(
+      allUniqPasswords.indexOf("skywalker") + 1,
+      allUniqPasswords.indexOf("obama2016")
+   );
+
+   const thirdPasswords = allUniqPasswords.slice(
+      allUniqPasswords.indexOf("obama2016") + 1
+   );
+
+   const firstAndSecondPassowords = firstPasswords.concat(secondPasswords);
+   const unacceptablePasswords = firstAndSecondPassowords.concat(
+      thirdPasswords
+   );
+   console.log(unacceptablePasswords);
+
    if (emailInput.length < 1) {
-      $("#enterEmail").removeClass("d-none");
+      $("#email-error").removeClass("d-none");
+      $("#email-error").html(" Please enter your email address.");
       $("#email-sign-up").addClass("is-invalid");
    } else {
-      $("#enterEmail").addClass("d-none");
+      $("#email-error").addClass("d-none");
       $("#email-sign-up").removeClass("is-invalid");
       // console.log(`The trimmed and normalized email is ${localEmail}.`);
    }
@@ -126,10 +146,9 @@ $("#letsGoButton").click(function () {
 
       // If the password is too short, then error message and red box but not other error messages
    } else if (passwordInput.length < 9) {
-      $("#passwordLenth").removeClass("d-none");
+      $("#password-error").html("Your password must be at least 9 characters.");
       $("#password-sign-up").addClass("is-invalid");
-      $("#enterPassword").addClass("d-none");
-      $("#differentPassword").addClass("d-none");
+      $("#password-error").removeClass("d-none");
 
       // if the password is the same as the first part/ local of the email address and the local part
       // is longer than 4 characters then error message and red box but not all other errors
@@ -137,14 +156,21 @@ $("#letsGoButton").click(function () {
       lowerCasedPassword.includes(localEmail) &&
       localEmail.length >= 4
    ) {
-      $("#differentPassword").removeClass("d-none");
+      $("#password-error").removeClass("d-none");
+      $("#password-error").html(
+         "All or part of your email address cannot be used in your password."
+      );
       $("#password-sign-up").addClass("is-invalid");
-      $("#passwordLenth").addClass("d-none");
-
+   } else if (unacceptablePasswords.includes(lowerCasedPassword)) {
+      $("#password-error").removeClass("d-none");
+      $("#password-error").html(
+         `Your password contains a commonly used password, ${passwordInput} can be easily discovered by attackers. Please use something else.`
+      );
+      $("#password-sign-up").addClass("is-invalid");
       // else no error messages or red boxes
    } else {
-      $("#enterPassword").addClass("d-none");
-      $("#passwordLenth").addClass("d-none");
+      $("#email-error").addClass("d-none");
+      $("#password-error").addClass("d-none");
       $("#differentPassword").addClass("d-none");
       $("#password-sign-up").removeClass("is-invalid");
    }
